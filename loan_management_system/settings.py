180ea5e7cb380737e142047e32f36c15afa3520c
Freddie16 <freddiemurigi@gmail.com>
2025-03-22 17:05:41 +0300

    updated hosts and env file

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
-
-# Application definition
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
